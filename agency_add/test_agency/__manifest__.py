0986e82c1e1b7b1a178ae9375a9bc172a7691071
Israel Tello <133825749+Mapple120600@users.noreply.github.com>
2025-11-27 11:10:05 -0500

    Add files via upload

---
+++ agency_add/test_agency/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Agencia Test',
+    'category': 'Test',
+    'summary': 'Modulo para agencia',
+    'version': '18.0.0.1',
+    'description': """Test""",
+    'author': 'Rapid Technologies SAC',
+    'website': 'https://www.rapid.tech',
+    'depends': ['base','sale', 'product'],
+    'data': [
+        'views/view.xml',
+    ],
+    'assets': {},
+    'installable': True,
+}
